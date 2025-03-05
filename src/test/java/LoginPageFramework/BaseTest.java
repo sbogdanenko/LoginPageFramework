c757b712661cba27ed796c8bc79adbcfd169baff
sbogdanenko <sbogdanenko@corcentric.com>
2025-03-05 14:43:30 +0100

    Added different browsers and parallel testing.

--- src/test/java/LoginPageFramework/BaseTest.java
+++ src/test/java/LoginPageFramework/BaseTest.java
@@ -3,10 +3,14 @@ package LoginPageFramework;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
@@ -15,13 +19,24 @@ public class BaseTest {
     protected static WebDriverWait wait;
 
     @BeforeClass
-    public void driverInitiate() {
+    @Parameters("browser")
+    public void driverInitiate(@Optional("chrome") String browser) {
         if (driver == null) {
-            driver = new ChromeDriver();
+            switch (browser.toLowerCase()) {
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+            }
             driver.manage().window().maximize();
             wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         }
     }
+
     @AfterClass
     public void driverClose() {
         if (driver != null) {
@@ -29,9 +44,11 @@ public class BaseTest {
             driver = null;
         }
     }
+
     protected void waitForElementToBeVisible(By locator) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
+
     protected void waitForElementToBeClickable(By locator) {
         wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
